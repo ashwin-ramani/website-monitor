3657b44baf81d7a0ee02d31d954c872ebac555a8
Ashwin Ramani <61066610+ashwin-ramani@users.noreply.github.com>
2022-02-18 16:24:52 -0500

    Update very_important.py

--- very_important.py
+++ very_important.py
@@ -1,8 +1,17 @@
-import random, requests, time, math, threading
+import random, requests, time, math, threading, json
+from replit import db
 
-data = {}
+with open("./codes.json") as file:
+	codes = json.load(file)
+
+data = json.loads(db["data"])
 workloads = [set()]
 
+def save():
+	while (True):
+		db["data"] = json.dumps(data)
+		time.sleep(5)
+	
 
 def lastw():
 	return len(workloads) - 1
@@ -20,24 +29,29 @@ def worker(i):
 	while (True):
 		for monitor_id in workloads[i]:
 			try:
-				if (requests.get(data[monitor_id]["url"]).status_code in (404, 405, 502)):
-					raise
-					
-				else:
-					if ("timestamp" in data[monitor_id]):
-						data[monitor_id].pop("timestamp")
-					
-					data[monitor_id]["uptime-length"] += 1
-					data[monitor_id]["readable-status"] = f"Website <b>{data[monitor_id]['url']}</b> is up."
+				status_code = requests.get(data[monitor_id]["url"]).status_code
 			except:
-				seconds = 0
-				if ("timestamp" in data[monitor_id]):
-					seconds = math.ceil(time.time() - data[monitor_id]["timestamp"])
-				else:
+				status_code = 404
+				
+			if (status_code in (404, 405, 502)):
+				time_ago = "0 seconds"
+				try:
+					time_ago = format_time(math.ceil(time.time() - data[monitor_id]["timestamp"]))
+				except:
 					data[monitor_id]["timestamp"] = time.time()
 
-				data[monitor_id]["readable-status"] = f"Website <b>{data[monitor_id]['url']}</b> is down - recorded as down {seconds} seconds ago."	
+				data[monitor_id]["raw-status"] = "down"
+				data[monitor_id]["readable-status"] = f"Page <b>{data[monitor_id]['url']}</b> is down - recorded as down {time_ago} ago."
+				
+			else:
+				if ("timestamp" in data[monitor_id]):
+					data[monitor_id].pop("timestamp")
+				
+				data[monitor_id]["uptime-length"] += 1
+				data[monitor_id]["readable-status"] = f"Page <b>{data[monitor_id]['url']}</b> is up."
+				data[monitor_id]["raw-status"] = "up"	
 
+			data[monitor_id]["status-code"] = f"{status_code} ({codes[str(status_code)]})"
 			data[monitor_id]["existance-length"] += 1
 
 		time.sleep(2)
@@ -58,3 +72,30 @@ def generate():
 		string = inner()
 
 	return string
+
+def format_time(seconds):
+	minutes = 0
+	hours = 0
+
+	while (seconds >= 60):
+		seconds -= 60
+		minutes += 1
+
+	while (minutes >= 60):
+		minutes -= 60
+		hours += 1
+
+	for unit in ("hours", "minutes"):
+		num = eval(unit)
+		if (num > 0):
+			return f"{num} {unit}"
+		
+	return f"{seconds} seconds"
+
+
+def start_threads():
+	threading.Thread(target = worker, args = (0,), daemon = True).start()
+	threading.Thread(target = save, daemon = True).start()
+
+	for monitor_id in data:
+		atw(monitor_id)
